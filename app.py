2fda4c9eb9ed0bcecac551a0c152c1f882f49596
tcruzyy <nugo.svianadze123@gmail.com>
2022-06-22 13:15:02 +0400

    Create New Route

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello Unilab!'
+    return 'Hello Unilab!!'
+
+@app.route('/new-route')
+def new_route():
+    return 'New route'
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
